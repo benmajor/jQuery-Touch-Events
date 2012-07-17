a948b7857ab566962d9aae2cde6760d7dc5b84ce
Ben Major <ben.major88@gmail.com>
2012-07-17 15:02:53 +0200

    Changed object reference to event reference as per bug report.

--- src/jquery.mobile-events.js
+++ src/jquery.mobile-events.js
@@ -221,7 +221,7 @@
 				var end_x = (settings.touch_capabale) ? e.targetTouches[0].pageX : e.pageX,
 					end_y = (settings.touch_capabale) ? e.targetTouches[0].pageY : e.pageY;
 				
-				if(origTarget == event.target && started && ((new Date().getTime() - start_time) < settings.taphold_threshold) && (start_pos.x == end_x && start_pos.y == end_y))
+				if(origTarget == e.target && started && ((new Date().getTime() - start_time) < settings.taphold_threshold) && (start_pos.x == end_x && start_pos.y == end_y))
 				{
 					triggerCustomEvent(thisObject, 'tap', e);
 				}
